19505283df89e7d40763cd290e852d5524285566
Chuanyan.Chen <chuanyan.chen@gmail.com>
2024-06-05 13:06:22 +0800

    NEW: 当用户输入摄氏度时，实时计算转换结果

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,15 @@
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using Avalonia;
+using System.ComponentModel;
 
 namespace GetStartedApp;
 
+[Decorator("MainWindow", 10)]
 public partial class MainWindow : Window
 {
     public MainWindow()
@@ -11,8 +17,40 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    public void ButtonClicked(object sender, RoutedEventArgs e)
+    public void btnCalculate_Click(object sender, RoutedEventArgs e)
     {
-        Debug.WriteLine("Clicked!");
+        Debug.WriteLine($"Clicked! Celsius={tbCelsius.Text}");
+        TypeInfo typeInfo = typeof(MainWindow).GetTypeInfo();
+        Console.WriteLine($"The assembley qualified name of MainWindow is {typeInfo.AssemblyQualifiedName}");
+        IEnumerable<Attribute> attrs = typeInfo.GetCustomAttributes();
+        foreach (Attribute attr in attrs)
+        {
+            Console.WriteLine($"Attribute on MainWindow: {attr.GetType().Name}");
+        }
+
+        TranlateCelsiusToFahrenheit();
+    }
+
+    public void tbCelsius_PropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs args)
+    {
+        if (args.Property == TextBox.TextProperty)
+        {
+            TranlateCelsiusToFahrenheit();
+        }
+    }
+
+    private void TranlateCelsiusToFahrenheit()
+    {
+        if (!Double.TryParse(tbCelsius.Text, out double C))
+        {
+            tbCelsius.Text = "0";
+        }
+        tbFahrenheit.Text = TranlateCelsiusToFahrenheit(C).ToString("0.0");
+    }
+
+    private static double TranlateCelsiusToFahrenheit(double C)
+    {
+        var F = C * (9d / 5d) + 32;
+        return F;
     }
 }
